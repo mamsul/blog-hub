01b7de8733c9720646ebad9a4dbd193c1b6bbf0f
mamsul <imamsulthoni.amd@gmail.com>
2024-01-17 11:20:07 +0700

    feat: redirect path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/post',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
