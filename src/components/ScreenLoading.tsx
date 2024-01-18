1c15222a5d75929c0d90c785659d10cec1568135
mamsul <imamsulthoni.amd@gmail.com>
2024-01-18 21:40:36 +0700

    feat: modify screen loading

--- src/components/ScreenLoading.tsx
+++ src/components/ScreenLoading.tsx
@@ -2,7 +2,7 @@ import HeaderLogo from '@/components/Header/HeaderLogo';
 
 const ScreenLoading = () => {
   return (
-    <div className="fixed inset-x-0 inset-y-0 z-20 bg-white/50 backdrop-blur-sm">
+    <div className="fixed inset-x-0 inset-y-0 z-20 bg-[#f9f4ec]">
       <div className="center-object h-full w-full flex-col">
         <HeaderLogo variant="loading" />
       </div>
